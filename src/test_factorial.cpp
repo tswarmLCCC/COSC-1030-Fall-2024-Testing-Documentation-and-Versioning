78a86b9def576514495e75b417b863588eebfc6b
Trevor Swarm <tswarm@lccc.wy.edu>
2024-10-03 10:05:33 -0600

    Update test_factorial.cpp

--- src/test_factorial.cpp
+++ src/test_factorial.cpp
@@ -1,14 +1,8 @@
-
-// test_factorial.cpp
-#include <cassert>
-#include "header.h"
-
-int main() {
-    assert(factorial(0) == 1);
-    assert(factorial(1) == 1);
-    assert(factorial(5) == 120);
-
-    // ... more tests ...
-
-    return 0;
+// source.cpp
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
 }
